fb7a228cdb41a3e2ec5328dbfc91f77990f01e57
JGomersall98 <91595142+JGomersall98@users.noreply.github.com>
2025-11-28 13:02:44 +0000

    added request show

--- Services/Media/IMediaService.cs
+++ Services/Media/IMediaService.cs
@@ -4,6 +4,8 @@ namespace MediaBridge.Services.Media
 {
     public interface IMediaService
     {
-        Task<StandardResponse> DownloadMovie(int tmbId, int userId, string username);
+        //Task<StandardResponse> DownloadMovie(int tmbId, int userId, string username, string mediaType);
+        //Task<StandardResponse> DownloadTVShow(int tmbId, int userId, string username, int[]? seasonsRequested, string mediaType);
+        Task<StandardResponse> DownloadMedia(int tmbId, int userId, string username, int[]? seasonsRequested, string mediaType);
     }
 }
