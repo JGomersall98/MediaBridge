595d60c554e342ce22125815e189cf5ad87c1f95
JGomersall98 <91595142+JGomersall98@users.noreply.github.com>
2026-01-04 17:07:37 +0000

    cleanup

--- Models/Media/ExternalServices/Sonarr/ShowDetailsResponse.cs
+++ Models/Media/ExternalServices/Sonarr/ShowDetailsResponse.cs
@@ -16,24 +16,3 @@ namespace MediaBridge.Models.Media.ExternalServices.Sonarr
         public int? ReleaseYear { get; set; }
     }
 }
-
-
-//public class RSGetMediaResponse
-//{
-//    [JsonPropertyName("id")]
-//    public int MediaId { get; set; }
-//    [JsonPropertyName("title")]
-//    public string? Title { get; set; }
-//    [JsonPropertyName("overview")]
-//    public string? Description { get; set; }
-//    [JsonPropertyName("tmdbId")]
-//    public int? TmdbId { get; set; } // Movie
-//    [JsonPropertyName("tvdbId")]
-//    public int? TvdbId { get; set; } // Tv Show
-//    [JsonPropertyName("imdbId")]
-//    public string? ImdbId { get; set; }
-//    [JsonPropertyName("remotePoster")]
-//    public string? PosterUrl { get; set; }
-//    [JsonPropertyName("year")]
-//    public int? ReleaseYear { get; set; }
-//}
